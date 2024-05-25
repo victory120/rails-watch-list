62439735a62c9899a86eb18f9a07a2c290693d38
victory agho <aghovictory120@gmail.com>
2024-05-25 03:30:00 -0700

    movies

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  root to: 'movies#index'
    # Defines the root path route ("/")
-  get "movies", to: "movies#index"
-  get "movies/:id", to: "movies#show"
+  resources :movies, only: [:show, :new, :create, :edit, :update, :destroy]
+  resources :lists, only: [:show, :new, :create, :edit, :update, :destroy]
 end
